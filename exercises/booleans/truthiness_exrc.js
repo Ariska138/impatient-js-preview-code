21a17c2c4c424739aea9b6c8e4787379abb6b5df
Ariska Hidayat <ariska138@gmail.com>
2019-04-05 20:54:26 +0700

    do boolean n calback

--- exercises/booleans/truthiness_exrc.js
+++ exercises/booleans/truthiness_exrc.js
@@ -4,17 +4,17 @@ Instructions:
 - Change the second parameters of assert.equal() so that the test passes
 */
 
-import {strict as assert} from 'assert';
+import {assert} from 'assert';
 
 test('instanceof', () => {
-  assert.equal(Boolean(null), 'some value');
-  assert.equal(Boolean(undefined), 'some value');
+  assert.equal(Boolean(null), false);
+  assert.equal(Boolean(undefined), false);
 
-  assert.equal(Boolean(''), 'some value');
-  assert.equal(Boolean('abc'), 'some value');
-  assert.equal(Boolean(0), 'some value');
-  assert.equal(Boolean(123), 'some value');
+  assert.equal(Boolean(''), false);
+  assert.equal(Boolean('abc'), true);
+  assert.equal(Boolean(0), false);
+  assert.equal(Boolean(123), true);
   
-  assert.equal(Boolean({}), 'some value');
-  assert.equal(Boolean([]), 'some value');
+  assert.equal(Boolean({}), true);
+  assert.equal(Boolean([]), true);
 });
